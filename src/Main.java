c9fd0e76649b96fb84d9ad7ebf628f01fee7e2c0
chris <christopher.gartner98@gmail.com>
2023-04-01 20:57:31 +0200

    implemented calculation for revenue and changed data type of revenue and profit to "long" instead of "double"

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import util.Calculator;
+
 /**
  * Main class for simulation
  */
 public class Main {
     public static void main(String[] args)
     {
+        Calculator calculator = new Calculator();
 
+        long revenue = calculator.calculateRevenue(313, 100213);
+        long profit  = calculator.calculateProfit(revenue, 1466);
+
+        System.out.println("Revenue: " + revenue);
+        System.out.println("Profit: " + profit);
     }
 }
--- src/util/Calculator.java
+++ src/util/Calculator.java
@@ -11,11 +11,24 @@ public class Calculator
      * @param revenue = the revenue of the company
      * @param costs = all comulated costs of the company
      *
-     * @return double
+     * @return long
      */
-    public double calculateProfit(double revenue, double costs)
+    public long calculateProfit(long revenue, long costs)
     {
         return revenue - costs;
+    }
+
+    /**
+     * calculates the revenue of a company
+     *
+     * @param pricePerUnit = the price per item that gets sold
+     * @param soldUnits = the total number of sold items
+     *
+     * @return double
+     */
+    public long calculateRevenue(long pricePerUnit, long soldUnits)
+    {
+        return pricePerUnit * soldUnits;
     }
 
 }
